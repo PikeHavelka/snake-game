2cc8c76fc8101d60598a55cf45ea1e70afc1d99d
Pavel Havelka <havelka.pvl@seznam.cz>
2025-06-28 18:14:10 +0200

    restart game function added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const score = document.getElementById("score");
+const finalScore = document.getElementById("finalScore");
+const dialog = document.getElementById("gameOverDialog");
+const restart = document.getElementById("restartGame");
 
 let lastDirection = "right"; // Snake default direction.
+let intervalID;
 
 let cellSize = 20; // Grid, snake and food size.
 let dx = cellSize; // Snake move;
@@ -12,6 +16,8 @@ let x = 0; // Start position of the Snake.
 let y = 0; // Start position of the Snake.
 let snakeTrail = [{ x, y }]; // Body position.
 let gameSpeed = 150; // Game speed/Snake speed.
+
+let yourScore = Number(score.textContent);
 
 let foodX = undefined; // Food coordinates.
 let foodY = undefined; // Food coordinates.
@@ -23,7 +29,7 @@ const foodPositionGeneration = () => {
 };
 
 // Change score/speed. Draw food.
-const whenAteFood = (interval) => {
+const whenAteFood = () => {
   ctx.fillStyle = "red";
   ctx.fillRect(foodX, foodY, cellSize, cellSize);
 
@@ -31,12 +37,11 @@ const whenAteFood = (interval) => {
     foodPositionGeneration();
 
     // Change game speed (difficulty)
-    gameSpeed -= 1;
-    clearInterval(interval);
+    gameSpeed -= 2;
+    clearInterval(intervalID);
     startGameLoop();
 
     // Change score
-    let yourScore = Number(score.textContent);
     yourScore += 1;
     score.textContent = yourScore;
   } else snakeTrail.pop();
@@ -126,24 +131,44 @@ const bodyCollision = () => {
       snakeTrail[0].x === snakeTrail[i].x &&
       snakeTrail[0].y === snakeTrail[i].y
     ) {
-      console.log("konec");
+      clearInterval(intervalID);
+      finalScore.textContent = score;
+      dialog.showModal();
     }
   }
 };
 
+restart.addEventListener("click", () => {
+  dialog.close();
+  restartGame();
+});
+
 const startGameLoop = () => {
-  const intervalID = setInterval(() => {
+  intervalID = setInterval(() => {
     backgroundGrid();
     snakeDraw();
     whenAteFood(intervalID);
-    
+
     ctx.fillStyle = "lime";
     snakeTrail.forEach((segment) => {
       ctx.fillRect(segment.x, segment.y, cellSize, cellSize);
     });
 
-    bodyCollision();
+    bodyCollision(intervalID);
   }, gameSpeed);
+};
+
+const restartGame = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  snakeTrail = [{ x, y }];
+  x = 0;
+  y = 0;
+
+  gameSpeed = 150;
+  score.textContent = yourScore = 0;
+  clearInterval(intervalID);
+  startGameLoop();
 };
 
 startGameLoop();
